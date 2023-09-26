5dc0580fde3e0555d1576262b3263b5111b4e238
iago <progiago@gmail.com>
2023-09-26 01:05:35 -0300

    wsgi

--- scrum_api/settings.py
+++ scrum_api/settings.py
@@ -82,6 +82,17 @@ WSGI_APPLICATION = 'scrum_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
@@ -92,12 +103,8 @@ WSGI_APPLICATION = 'scrum_api.wsgi.application'
 #         "PORT": config('DB_PORT'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- scrum_api/wsgi.py
+++ scrum_api/wsgi.py
@@ -15,3 +15,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrum_api.settings')
 
 application = get_wsgi_application()
+
+
+path = '/home/cardoso14/IntegradorV1/scrum_api'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+
+application = get_wsgi_application()
