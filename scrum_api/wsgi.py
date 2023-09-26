f7384b2cad1cfb4f1b254c63bcf8ebf7e79e46d6
iago <progiago@gmail.com>
2023-09-26 01:01:07 -0300

    +1

--- scrum_api/wsgi.py
+++ scrum_api/wsgi.py
@@ -15,13 +15,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrum_api.settings')
 
 application = get_wsgi_application()
-
-
-path = '/home/myusername/mysite'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-
-application = get_wsgi_application()
