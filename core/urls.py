82a3216c1bc4b69c6f1f6d81a33ce88ea0fb3d49
iago <progiago@gmail.com>
2023-09-29 03:21:19 -0300

    improvement

--- core/urls.py
+++ core/urls.py
@@ -5,16 +5,16 @@ from core.api import api
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', api.List_profile, name="api"),
-    path('<int:pk>', api.Detail_profile, name="api"),
-    path('project/', api.Project_List, name="api"),
-    path('project/<int:pk>', api.Detail_project, name="api"),
-    path('equipe/', api.Team_List, name="api"),
-    path('equipe/<int:pk>', api.detail_equipe, name="api"),
-    path('tarefa/', api.tarefa_list, name="api"),
-    path('tarefa/<int:pk>', api.detail_tarefa, name="api"),
-    path('checklist/', api.checklist_list, name="api"),
-    path('checklist/<int:pk>', api.detail_checklist, name="api"),
-    path('comentarios/', api.comentario_list, name="api"),
-    path('comentario/<int:pk>', api.detail_comentario, name="api"),
+    path('', api.List_profile.as_view()),
+    path('<int:pk>', api.Detail_profile.as_view()),
+    path('project/', api.Project_List.as_view()),
+    path('project/<int:pk>', api.Detail_project.as_view()),
+    path('equipe/', api.Team_List.as_view()),
+    path('equipe/<int:pk>', api.Detail_Team.as_view()),
+    path('tarefa/', api.Task_List.as_view()),
+    path('tarefa/<int:pk>', api.Detail_Task.as_view()),
+    path('checklist/', api.Checklist_List.as_view()),
+    path('checklist/<int:pk>', api.Detail_Task.as_view()),
+    path('comentarios/', api.Comment_List.as_view()),
+    path('comentario/<int:pk>', api.Detail_Comment.as_view()),
 ]
