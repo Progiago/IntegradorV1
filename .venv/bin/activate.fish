c233533f5f51ed012ce9b138c980ab1279a00ade
iago cardoso do nascimento <iago@iago.iago>
2023-09-30 20:26:43 -0300

    improvement2

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/iago/Downloads/pi/FinalV1/.venv"
+set -gx VIRTUAL_ENV "/home/iago/Downloads/pi/IntegradorV1/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
